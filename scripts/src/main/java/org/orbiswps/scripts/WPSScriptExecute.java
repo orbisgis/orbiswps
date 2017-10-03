083d3b079632e866f47659f4d8b5ff31bc399925
ebocher <erwan.bocher@univ-ubs.fr>
2017-10-03 18:56:34 +0200

    Prepare WPS script tests

---
+++ scripts/src/main/java/org/orbiswps/scripts/WPSScriptExecute.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2017 Lab-STICC - UMR CNRS 6285
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.orbiswps.scripts;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Map;
+import org.codehaus.groovy.control.CompilationFailedException;
+
+/**
+ * @author Sylvain Palominos
+ * @author Erwan Bocher
+ */
+public class WPSScriptExecute {
+
+      /**
+     * This method is used to test the groovy script execution. The given script is parsed ans the configured with the
+     * given inputs and properties. Ons the method processing is run, the script output are tested with the given one.
+     * @param groovyClassLoader 
+     * @param scriptPath Path to the script to run.
+     * @param inputMap Map containing the inputs. The keys are the script attribute name and the values are the attribute data.
+     * @param propertyMap Map containing the groovy properties. The keys are the property attribute name and the values
+     *                    are the property attribute data.
+     * @param outputMap Map containing the desired outputs. The keys are the script output attribute name and the values
+     *                  are the desired output attribute data which will be tested.
+     * @throws java.lang.Exception
+     */
+    public static  void run(GroovyClassLoader groovyClassLoader, String scriptPath,
+                                    Map<String, Object> propertyMap,
+                                    Map<String, Object> inputMap,
+                                    Map<String, Object> outputMap) throws Exception{
+        //Step 1 : Parse the script
+        Class scriptClass = null;
+        try {
+            File groovyFile = new File(scriptPath);
+            groovyClassLoader.clearCache();
+            scriptClass = groovyClassLoader.parseClass(groovyFile);
+        } catch (IOException | CompilationFailedException e) {
+            throw new Exception("Can not parse the script '"+scriptPath+"'\n Cause : "+e.getLocalizedMessage());
+        }
+        
+        //Step 2 : Create the groovy object
+        GroovyObject groovyObject = null;
+        try {
+            groovyObject = (GroovyObject) scriptClass.newInstance();
+        } catch (InstantiationException|IllegalAccessException e) {
+            throw new Exception("Can not create the groovy object of '"+scriptPath+"'\n Cause : "+e.getLocalizedMessage());
+        }
+        
+        if(scriptClass==null){
+            throw new Exception("The groovy object of '"+scriptPath+"' can not be null.");
+        }
+
+        //Step 3 : Sets the groovy object
+        for(Map.Entry<String, Object> entry : inputMap.entrySet()){
+            Field f = null;
+            try {
+                f = scriptClass.getDeclaredField(entry.getKey());
+                if(f != null){
+                    f.setAccessible(true);
+                    f.set(groovyObject, entry.getValue());
+                }
+                else{
+                    throw new Exception("Unable to get the field '"+entry.getKey()+"' from the script '"+ scriptPath+"'.");
+                }
+            } catch (NoSuchFieldException e) {
+                throw new Exception("Unable to get the field '"+entry.getKey()+"' from the script '"+scriptPath+"'\n Cause : "+
+                        e.getLocalizedMessage());
+            } catch (IllegalAccessException e) {
+                throw new Exception("Unable to set the field '"+entry.getKey()+"' from the script '"+scriptPath+"'\n Cause : "+
+                        e.getLocalizedMessage());
+            }
+        }
+        for(Map.Entry<String, Object> entry : propertyMap.entrySet()) {
+            groovyObject.setProperty(entry.getKey(), entry.getValue());
+        }
+
+        //Step 4 : Run the processing method
+        groovyObject.invokeMethod("processing", null);
+
+        //Step 5 : Test the outputs
+        for(Map.Entry<String, Object> entry : outputMap.entrySet()){
+            Field f = null;
+            try {
+                f = scriptClass.getDeclaredField(entry.getKey());
+                if(f != null){
+                    f.setAccessible(true);
+                    if(!entry.getValue().equals(f.get(groovyObject))){
+                     throw new Exception("The field '"+f.getName()+"' hasn't the expected value.");
+                    }
+                }
+                else{    
+                    throw new Exception("Unable to get the field '" + entry.getKey() + "' from the script '"
+                            + scriptPath + "'.");
+                }
+            } catch (IllegalAccessException|NoSuchFieldException e) {
+                throw new Exception("Unable to get the field '"+entry.getKey()+"' from the script '"+scriptPath+"'\n Cause : "+
+                        e.getLocalizedMessage());
+            }
+        }
+    }
+    
+}
---
+++ scripts/src/test/java/org/orbiswps/scripts/WPSScriptTests.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2017 Lab-STICC - UMR CNRS 6285
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.orbiswps.scripts;
+
+import groovy.lang.GroovyClassLoader;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import org.h2gis.functions.factory.H2GISDBFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Erwan Bocher
+ */
+public class WPSScriptTests {
+
+    private static Connection connection;
+    private GroovyClassLoader groovyClassLoader = new GroovyClassLoader(this.getClass().getClassLoader());
+
+    @BeforeClass
+    public static void tearUp() throws Exception {
+        // Keep a connection alive to not close the DataBase on each unit test
+        connection = H2GISDBFactory.createSpatialDataBase(WPSScriptTests.class.getSimpleName());
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        connection.close();
+    }
+    private Statement st;
+
+    @Before
+    public void setUpStatement() throws Exception {
+        st = connection.createStatement();
+    }
+
+    @After
+    public void tearDownStatement() throws Exception {
+        st.close();
+    }
+
+    @Test
+    public void testBasicScript() throws Exception {
+        String scriptPath = "/home/sylvain/Bureau/testScript.groovy";
+        Map<String, Object> inputMap = new HashMap<>();
+        inputMap.put("inStr", "good ");
+        Map<String, Object> propertyMap = new HashMap<>();
+        propertyMap.put("str", "value");
+        Map<String, Object> outputMap = new HashMap<>();
+        outputMap.put("outStr", "good value");
+        WPSScriptExecute.run(groovyClassLoader, scriptPath, propertyMap, inputMap, outputMap);
+
+    }
+
+}
